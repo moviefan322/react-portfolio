cc4d5d992989b934e872ddace0deba88c46079d5
moviefan322 <philman202@yahoo.com>
2023-04-10 16:42:31 -0400

    adds fundamental structure

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="#">Github</a>
+        </li>
+        <li>
+          <a href="#">LinkedIn</a>
+        </li>
+        <li>
+          <a href="#">Another Link</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          Logo
+        </a>
+        <ul>
+          <li>Philip Neumann</li>
+          <li>About Me</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
