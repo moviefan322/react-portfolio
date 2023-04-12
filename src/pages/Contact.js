f7aab3c9ac1d2df0321008120d976d62647b9456
moviefan322 <philman202@yahoo.com>
2023-04-11 20:04:06 -0400

    adds contact/portfolio pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div id='contact'>
+      <h1>Contact Me!</h1>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
