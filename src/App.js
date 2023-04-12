737fe96a794ddc3156ca63f7f8db555af303a2af
moviefan322 <philman202@yahoo.com>
2023-04-11 21:17:54 -0400

    adds full functionality to navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import "./assets/css/App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer";
 import About from "./pages/About.js";
+import Portfolio from "./pages/Portfolio.js";
+import Contact from "./pages/Contact.js";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <About />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route
+            path="/portfolio"
+            element={<Portfolio />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,27 @@
 import logo from "../assets/images/logo.png";
+import resume from "../assets/docs/Resume.pdf";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [currentPage, setCurrentPage] = useState("About Me");
+
+  useEffect(() => {
+    const storedPage = localStorage.getItem("currentPage");
+    if (storedPage) {
+      setCurrentPage(storedPage);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.title = `${currentPage} | Philip Neumann}`;
+  }, [currentPage]);
+
+  const handleLinkClick = (page) => {
+    setCurrentPage(page);
+    localStorage.setItem("currentPage", page);
+  };
+
   return (
     <nav>
       <div className="App-header">
@@ -12,10 +33,40 @@ const Header = () => {
         />
         <h1 id="name">Philip Neumann</h1>
         <ul id="navlinks">
-          <li>About Me</li>
-          <li>Portfolio</li>
-          <li>Contact</li>
-          <li>Resume</li>
+          <Link
+            to="/"
+            style={
+              currentPage === "About"
+                ? { color: "yellow" }
+                : { color: "#FFFFFF" }
+            }
+            onClick={() => handleLinkClick("About")}
+          >
+            About Me
+          </Link>
+          <Link
+            to="/portfolio"
+            style={
+              currentPage === "Portfolio"
+                ? { color: "yellow" }
+                : { color: "#FFFFFF" }
+            }
+            onClick={() => handleLinkClick("Portfolio")}
+          >
+            Portfolio
+          </Link>
+          <Link
+            to="/contact"
+            style={
+              currentPage === "Contact"
+                ? { color: "yellow" }
+                : { color: "#FFFFFF" }
+            }
+            onClick={() => handleLinkClick("Contact")}
+          >
+            Contact
+          </Link>
+          <a href={resume}>Resume</a>
         </ul>
       </div>
     </nav>
