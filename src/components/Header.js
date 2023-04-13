ddfe7504c120fd5e8f7d894ab680957dc66eac89
moviefan322 <philman202@yahoo.com>
2023-04-13 17:54:50 -0400

    fixes mobile query portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const Header = () => {
-  const [currentPage, setCurrentPage] = useState("About");
+  const [currentPage, setCurrentPage] = useState("");
 
   useEffect(() => {
     document.title = `${currentPage} | Philip Neumann}`;
